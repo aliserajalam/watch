4927238640c3b854379e23fd9f90b44931ec888a
Ali Alam <contact@aliserajalam.com>
2020-09-27 13:38:49 +1000

    Added FAQs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron.js';
 import { FooterContainer } from './containers/footer.js';
+import { FaqsContainer } from './containers/faqs.js';
 
 function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
